ace99abecf9466faab14945e1d1b10c956c8093a
lelderbe <fvdc@ya.ru>
2023-02-13 19:20:44 +0300

    init: initial commit

---
+++ task09.js
@@ -0,0 +1,10 @@
+const arr = [
+  { name: 'Joe', age: 21 },
+  { name: 'Mary', age: 18 },
+  { name: 'Karen', age: 31 },
+  { name: 'Steve', age: 27 },
+  { name: 'Kerrigan', age: 33 },
+];
+
+// найди объект, у которого возраст 31 и выведи его в консоль
+// используй метод массивов find()
