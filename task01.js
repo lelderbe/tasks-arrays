8878062c391dddd55429a3a20c7a8a5cee36a7c4
evikary <daria_more10@mail.ru>
2023-02-14 10:14:16 +0300

    feat: add task01 solution

--- task01.js
+++ task01.js
@@ -3,8 +3,20 @@ const arr = [1, 2, 3, 4, 5];
 // выведи в консоль все элементы массива
 // используй цикл for..i
 
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
 // выведи в консоль все элементы массива
 // используй цикл for..of
 
+for (let item of arr) {
+  console.log(item);
+}
+
 // выведи в консоль все элементы массива
 // используй метод forEach()
+
+arr.forEach((item) => {
+  console.log(item);
+});
