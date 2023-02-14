89caa1bec7fa90f53460985ab7097f2f087a5b5c
evikary <daria_more10@mail.ru>
2023-02-14 14:15:49 +0300

    feat: add task05 solution

--- task05.js
+++ task05.js
@@ -1,3 +1,18 @@
 const message = 'I have a pen';
 
 // разверни строку задом наперёд и выведи её в консоль
+
+// применить метод split для того, чтобы перевести строку в массив букв
+// применить метод reverse к массиву, чтобы развернуть массив
+// применить метод join, чтобы преобразовать массив в строку
+// вывести результат в консоль
+
+const myArr = message.split('');
+
+console.log(myArr.reverse());
+
+console.log(myArr.join(''));
+
+/////////////
+
+console.log(message.split('').reverse().join(''));
