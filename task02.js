6fef7ec98facaf5b64fb3c53da6c315f011bb310
evikary <daria_more10@mail.ru>
2023-02-14 10:50:42 +0300

    feat: add task02 solution

--- task02.js
+++ task02.js
@@ -3,8 +3,26 @@ const arr = [1, 2, 3, 4, 5];
 // выведи только нечётные элементы массива
 // используй цикл for..i
 
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 !== 0) {
+    console.log(arr[i]);
+  }
+}
+
 // выведи только нечётные элементы массива
 // используй цикл for..of
 
+for (let item of arr) {
+  if (item % 2 === 1) {
+    console.log(item);
+  }
+}
+
 // выведи только нечётные элементы массива
 // используй метод forEach()
+
+arr.forEach((item) => {
+  if (item % 2 !== 0) {
+    console.log(item);
+  }
+});
