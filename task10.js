be10b9f329a49b008e47e74baaf87b5452074313
evikary <daria_more10@mail.ru>
2023-02-15 15:53:40 +0300

    feat: add task10 solution

--- task10.js
+++ task10.js
@@ -2,3 +2,11 @@ const message = "Time flies when you're having fun";
 
 // выведи новый массив, который будет содержать слова из строки message, длина которых меньше 5
 // используй метод filter() и прочие, которые сочтёшь нужными
+
+const myArr = message.split(' ');
+// console.log(myArr);
+
+myArrFilter = myArr.filter((item) => {
+  return item.length < 5;
+});
+console.log(myArrFilter);
